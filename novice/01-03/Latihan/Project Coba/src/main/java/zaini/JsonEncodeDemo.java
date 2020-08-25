d39dec5bd0d47b39ba2a005566b5301acfc00d25
zaini24 <zainibanget@gmail.com>
2020-08-25 16:41:07 +0700

    ngepush lagi cuy

---
+++ novice/01-03/Latihan/Project Coba/src/main/java/zaini/JsonEncodeDemo.java
@@ -0,0 +1,43 @@
+// package Json;
+// import java.util.HashMap;
+// import com.google.gson.Gson;
+// public class JsonEncodeDemo {
+//     private final Gson serializer = new Gson();
+//     public static void main(String[] args){
+//         new JsonEncodeDemo().mulai();
+//     }
+//     public void mulai() {
+//         HashMap<String, Object> data = new HashMap<>();
+//         data.put("name","foo");
+//         data.put("num", 100);
+//         data.put("balance", 1000.21);
+//         data.put("is_vip", true);
+//         String json = this.serializer.toJson(data);
+//         System.out.print(json);
+//     }
+// }
+package zaini;
+
+import java.util.HashMap;
+import com.google.gson.Gson;
+
+public class JsonEncodeDemo {
+
+    private final Gson serializer = new Gson();
+    
+    public static void main(String[] args){
+        new JsonEncodeDemo().mulai();
+    }
+    public void mulai() {
+        HashMap<String, Object> data = new HashMap<>();
+
+        data.put("name","foo");
+        data.put("num", 100);
+        data.put("balance", 1000.21);
+        data.put("is_vip", true);
+
+        String json = this.serializer.toJson(data);
+        System.out.print(json);
+    }
+
+}
