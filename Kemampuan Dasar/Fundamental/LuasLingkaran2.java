7cb26c4eed9c83af97c61e74ab84d5e911472e32
zaini24 <zainibanget@gmail.com>
2020-08-26 23:24:26 +0700

    update hari ketiga minggu kedua

---
+++ Kemampuan Dasar/Fundamental/LuasLingkaran2.java
@@ -0,0 +1,22 @@
+package pertemuan1;
+
+public class LuasLingkaran2 {
+    
+    public static void main(string[] args) {
+        
+        // Deklarasi
+        Double pi, r, luas;
+
+        // Deskripsi
+        r = 10.0;
+        pi = 3.14;
+
+        // Proses
+        luas = pi * r * r;
+
+        // Output
+        System.out.print("Luas Lingkaran = ");
+        System.out.println(luas);
+    }
+    |
+}
