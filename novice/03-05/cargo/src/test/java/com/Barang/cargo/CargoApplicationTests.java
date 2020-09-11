f00da5b59f96071275cf67e0fe4612f7e04e7456
Zaini <zainibanget@gmail.com>
2020-09-11 18:14:23 +0700

    CRUD!

---
+++ novice/03-05/cargo/src/test/java/com/Barang/cargo/CargoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Barang.cargo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ novice/03-05/java script/Latihan.js
@@ -0,0 +1,16 @@
+
+console.log('Saya sedang belajar java script')
+
+console.log('Belajar Nodejs')
+
+var nama = 'Praxis Academy'
+console.log(`woy kuliah di ${nama.top}`)
+
+var namo = 'Asli loe'
+
+// cara membuat fungsi. ikuti alur seperti dibawah ini ya gaes. oh iya, kamu hanya bisa manggil satu variabel aja loh ya
+function hitung (){
+    console.log(`gaul ${nama}`)
+}
+// untuk memanggil fungsinya, kamu harus buat nama fungsinya kemudian buat buka tutup kurung.
+hitung()
